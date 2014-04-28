f9ec8bde8fe76a3bc10bf50ddf8567cf00956b95
Jonathan Ni <jonathan.r.ni@gmail.com>
2014-04-28 11:04:33 -0400

    Add Mixing Algorithm and Mersenne Twister PRNG
    Add getter and setter for correct question ID.
    
    Implement mixing algorithm (Fisher-Yates, Knuth version). Uses Mersenne
    Twister PRNG (Pseudo-Random Number Generator) as a randomizer. This has
    the ability to have a very long period, as well as random distribution.
    
    The MersenneTwisterFast.java file was added to another package, ec.util
    which will be included in the regular src folder but not the src-client
    folder.

--- ClientProject/src/com/esf/tm/MCQuestion.java
+++ ClientProject/src/com/esf/tm/MCQuestion.java
@@ -1,6 +1,9 @@
 package com.esf.tm;
 
 import java.util.ArrayList;
+import java.util.Collections;
+
+import ec.util.MersenneTwisterFast;
 
 /**
  * 
@@ -8,87 +11,160 @@ import java.util.ArrayList;
  * 
  * @author Jonathan Ni
  * @since 4/22/14
- * @version 0.0r3
+ * @version 0.0r4
  * 
  */
 
 class MCQuestion extends Question
 {
-	private ArrayList<Choice> choiceList = new ArrayList<Choice>();
-	private int correctAnswer;
+    private ArrayList<Choice> choiceList = new ArrayList<Choice>();
+    private int correctAnswer;
 
-	/**
-	 * 
-	 * See {@link Question#Question(String, int)}
-	 * 
-	 * @param message
-	 * @param ID
-	 */
+    /**
+     * 
+     * See {@link Question#Question(String, int)}
+     * 
+     * @param message
+     * @param ID
+     */
 
-	public MCQuestion(String message, int ID)
+    public MCQuestion(String message, int ID)
+    {
+	super(message, ID);
+    }
+
+    /**
+     * 
+     * Adds a choice to the question.
+     * 
+     * @param choice
+     *            the choice
+     */
+
+    void addChoice(Choice choice)
+    {
+	choiceList.add(choice);
+    }
+
+    /**
+     * 
+     * Sets a choice in the choice list to a new choice. If the index is out of
+     * bounds the method throws an IllegalArgumentException.
+     * 
+     * @param index
+     *            the index of the choice
+     * @param choice
+     *            the new choice
+     * 
+     * @throws IllegalArgumentException
+     */
+
+    void setChoice(int index, Choice choice)
+    {
+	if (index < 0 || index >= choiceList.size())
+	    try
+	    {
+		throw new IllegalArgumentException("Index out of bounds");
+	    } catch (IllegalArgumentException e)
+	    {
+		ErrorReporter.reportError("Choice index out of bounds",
+			Util.stackTraceToString(e));
+	    }
+
+	choiceList.set(index, choice);
+    }
+
+    /**
+     * 
+     * Removes a choice from the list of choices. If the index is out of bounds
+     * the method throws an IllegalArgumentException.
+     * 
+     * @param index
+     *            the index of the choice
+     * 
+     * @throws IllegalArgumentException
+     */
+
+    void removeChoice(int index)
+    {
+	if (index < 0 || index >= choiceList.size())
+	    try
+	    {
+		throw new IllegalArgumentException("Index out of bounds");
+	    } catch (IllegalArgumentException e)
+	    {
+		ErrorReporter.reportError("Choice index out of bounds",
+			Util.stackTraceToString(e));
+	    }
+
+	choiceList.remove(index);
+    }
+
+    /**
+     * 
+     * Gets the ID of the correct answer.
+     * 
+     * @return the ID
+     */
+
+    public int getCorrectAnswer()
+    {
+	return correctAnswer;
+    }
+
+    /**
+     * 
+     * Sets the ID of the correct answer. Should be performed after choices are
+     * assigned IDs.
+     * 
+     * @param correctAnswer
+     */
+
+    public void setCorrectAnswer(int correctAnswer)
+    {
+	this.correctAnswer = correctAnswer;
+    }
+
+    /**
+     * 
+     * Assigns the IDs to the choices in order. This is necessary because when
+     * choices are added to the question, their ID should be default 0. Right
+     * before mixing, the driver program needs to assign the IDs to the choices,
+     * then mix the question as many times as it wants. This ensures that the
+     * original ordering is not lost.
+     * 
+     */
+
+    void assignIDs()
+    {
+	for (int i = 0; i < choiceList.size(); i++)
+	    choiceList.get(i).setChoiceID(i);
+    }
+
+    /**
+     * 
+     * Shuffle the ordering of the choices, preserving their IDs. The function
+     * {@link #assignIDs()} should be called before this function to ensure that
+     * IDs are properly assigned.
+     * 
+     * Uses the Fisher-Yates algorithm for shuffling with the Mersenne Twister
+     * PRNG implementation. See ec.util.MersenneTwisterFast for copyright
+     * details.
+     * 
+     * Fisher-Yates:
+     * 
+     * To shuffle an array a of n elements (indices 0..n-1): for i from n − 1
+     * downto 1 do j = random integer with 0 <= j <= i exchange a[j] and a[i]
+     */
+
+    void mix()
+    {
+	MersenneTwisterFast prng = new MersenneTwisterFast();
+
+	for (int i = choiceList.size() - 1; i > 0; i--)
 	{
-		super(message, ID);
+	    int j = prng.nextInt(i + 1);
+	    Collections.swap(choiceList, i, j);
 	}
-
-	/**
-	 * 
-	 * Adds a choice to the question.
-	 * 
-	 * @param choice
-	 *            the choice
-	 */
-
-	void addChoice(Choice choice)
-	{
-
-	}
-
-	/**
-	 * 
-	 * Sets a choice in the choice list to a new choice.
-	 * 
-	 * @param index
-	 *            the index of the choice
-	 * @param choice
-	 *            the new choice
-	 */
-
-	void setChoice(int index, Choice choice)
-	{
-
-	}
-
-	/**
-	 * 
-	 * Removes a choice from the list of choices
-	 * 
-	 * @param index
-	 *            the index of the choice
-	 */
-
-	void removeChoice(int index)
-	{
-
-	}
-
-	/**
-	 * 
-	 * Assigns the IDs to the choices in order. This is necessary because when
-	 * choices are added to the question, their ID should be default 0. Right
-	 * before mixing, the driver program needs to assign the IDs to the choices,
-	 * then mix the question as many times as it wants. This ensures that the
-	 * original ordering is not lost.
-	 * 
-	 */
-
-	void assignIDs()
-	{
-		for (int i = 0; i < choiceList.size(); i++)
-			choiceList.get(i).setChoiceID(i);
-	}
-
-	void mix()
-	{
-
-	}
+    }
 }
