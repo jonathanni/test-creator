90ca26ee1aab3a359c7da2623735c75e4bb1be99
Jonathan Ni <jonathan.r.ni@gmail.com>
2014-05-01 10:44:38 -0400

    Implement Setting Correct Answer Choices for T/F Questions

--- ClientProject/src/com/esf/tm/TFQuestion.java
+++ ClientProject/src/com/esf/tm/TFQuestion.java
@@ -13,16 +13,44 @@ package com.esf.tm;
 public class TFQuestion extends Question
 {
 
-	/**
-	 * 
-	 * See {@link Question#Question(String, int)}
-	 * 
-	 * @param message
-	 * @param ID
-	 */
+    private boolean correctAnswer;
 
-	public TFQuestion(String message, int ID)
-	{
-		super(message, ID);
-	}
+    /**
+     * 
+     * See {@link Question#Question(String, int)}
+     * 
+     * @param message
+     * @param ID
+     */
+
+    public TFQuestion(String message, int ID)
+    {
+	super(message, ID);
+    }
+
+    /**
+     * 
+     * Gets the correct answer as a boolean, since this is a T/F question.
+     * 
+     * @return the answer
+     */
+
+    public boolean isCorrectAnswer()
+    {
+	return correctAnswer;
+    }
+
+    /**
+     * 
+     * Sets the correct answer to this question as a boolean, since this is a
+     * T/F question.
+     * 
+     * @param correctAnswer
+     *            the answer
+     */
+
+    public void setCorrectAnswer(boolean correctAnswer)
+    {
+	this.correctAnswer = correctAnswer;
+    }
 }
