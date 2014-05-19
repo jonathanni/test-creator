75a4c022e46cf88bdd0a72a5918f447af82967a2
Jonathan Ni <jonathan.r.ni@gmail.com>
2014-05-19 10:50:17 -0400

    Add Choice Addition and Blank Addition to QuestionEditor
    QuestionEditor now has multiple choice addition capabilities. The 
    majority of the code was copied from TestGenerator and reformatted to
    work with the multiple choices.
    
    Blank addition means that blanks can be added and deleted, but not 
    moved nor edited.

--- ClientProject/src/com/esf/tm/QuestionEditor.java
+++ ClientProject/src/com/esf/tm/QuestionEditor.java
@@ -1,11 +1,16 @@
 package com.esf.tm;
 
-import java.awt.CardLayout;
+import java.awt.Component;
 import java.util.ArrayList;
+import java.util.Collections;
 
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JSpinner;
@@ -15,6 +20,8 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import org.javabuilders.BuildResult;
 import org.javabuilders.swing.SwingJavaBuilder;
@@ -30,217 +37,430 @@ import org.javabuilders.swing.SwingJavaBuilder;
  * 
  */
 
-public class QuestionEditor extends JFrame implements ChangeListener
+public class QuestionEditor extends JFrame implements ChangeListener,
+	ListSelectionListener
 {
-	private static final long serialVersionUID = -8403774668066080746L;
-	private BuildResult result;
+    private static final long serialVersionUID = -8403774668066080746L;
+    private BuildResult result;
 
-	private CardLayout questionLayout;
-	private JLabel questionLabel;
-	private JPanel questionContainer;
-	private JTextArea questionDescription;
-	private JRadioButton rb1, rb2, rb3, rbTrue;
-	private JCheckBox mixable, caseSensitive, whitespaceSensitive;
-	private JSpinner blankSpaces;
+    private CustomCardLayout customLayout;
+    private JLabel questionLabel;
+    private JPanel tquestionContainer, questionContainer;
+    private JTextArea questionDescription;
+    private JRadioButton rb1, rb2, rb3, rbTrue;
+    private JCheckBox questionMixable, caseSensitive, whitespaceSensitive;
+    private JSpinner blankSpaces;
+    private JButton editC, addC, remC, moveUpC, moveDownC, editB, addB, remB,
+	    moveUpB, moveDownB, editCA, addCA, remCA, moveUpCA, moveDownCA;
+    private JList mcChoices, fibBlankList, fibBlankCAChoicesList;
+    private DefaultListModel mcCListModel = new DefaultListModel(),
+	    fibBlankListModel = new DefaultListModel(),
+	    fibCAListModel = new DefaultListModel();
 
-	private final Question question;
+    private final Question question;
 
-	private ArrayList<Choice> mcQChoices = new ArrayList<Choice>();
-	private int mcCorrectAnswer;
+    private ArrayList<Choice> mcQChoices = new ArrayList<Choice>();
+    private int mcCorrectAnswer, mcChoiceIndex = -1, mcChoiceCount;
 
-	private ArrayList<ArrayList<String>> fibQcorrectAnswers = new ArrayList<ArrayList<String>>();
+    private ArrayList<ArrayList<String>> fibQcorrectAnswers = new ArrayList<ArrayList<String>>();
+    private int fibBlankIndex = -1, fibBlankCount, fibCOIndex = -1, fibCOCount;
 
-	/**
-	 * 
-	 * Creates a new QuestionEditor and binds a Question to it. This
-	 * QuestionEditor will change the contents of the question binded.
-	 * 
-	 * @param question
-	 *            the Question to bind
-	 */
+    /**
+     * 
+     * Creates a new QuestionEditor and binds a Question to it. This
+     * QuestionEditor will change the contents of the question binded.
+     * 
+     * @param question
+     *            the Question to bind
+     */
 
-	public QuestionEditor(Question question)
+    public QuestionEditor(Question question)
+    {
+	this.question = question;
+
+	result = SwingJavaBuilder.build(this);
+
+	try
 	{
-		this.question = question;
-
-		result = SwingJavaBuilder.build(this);
-
-		try
-		{
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (ClassNotFoundException e)
-		{
-			ErrorReporter.reportError(
-					"An error has occured while trying to open the window.",
-					Util.stackTraceToString(e));
-			e.printStackTrace();
-		} catch (InstantiationException e)
-		{
-			ErrorReporter.reportError(
-					"An error has occured while trying to open the window.",
-					Util.stackTraceToString(e));
-			e.printStackTrace();
-		} catch (IllegalAccessException e)
-		{
-			ErrorReporter.reportError(
-					"An error has occured while trying to open the window.",
-					Util.stackTraceToString(e));
-			e.printStackTrace();
-		} catch (UnsupportedLookAndFeelException e)
-		{
-			ErrorReporter.reportError(
-					"An error has occured while trying to open the window.",
-					Util.stackTraceToString(e));
-			e.printStackTrace();
-		}
-
-		questionDescription.setText(getQuestion().getMessage());
-		(getQuestion() instanceof FIBQuestion ? rb3
-				: (getQuestion() instanceof TFQuestion ? rb2 : rb1))
-				.setSelected(true);
-		blankSpaces.setModel(new SpinnerNumberModel(1, 1, 255, 1));
-		blankSpaces.addChangeListener(this);
-
-		// By default, the first is selected and is the "correct answer"
-		addChoice();
-
-		setVisible(true);
-		pack();
+	    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+	} catch (ClassNotFoundException e)
+	{
+	    ErrorReporter.reportError(
+		    "An error has occured while trying to open the window.",
+		    Util.stackTraceToString(e));
+	    e.printStackTrace();
+	} catch (InstantiationException e)
+	{
+	    ErrorReporter.reportError(
+		    "An error has occured while trying to open the window.",
+		    Util.stackTraceToString(e));
+	    e.printStackTrace();
+	} catch (IllegalAccessException e)
+	{
+	    ErrorReporter.reportError(
+		    "An error has occured while trying to open the window.",
+		    Util.stackTraceToString(e));
+	    e.printStackTrace();
+	} catch (UnsupportedLookAndFeelException e)
+	{
+	    ErrorReporter.reportError(
+		    "An error has occured while trying to open the window.",
+		    Util.stackTraceToString(e));
+	    e.printStackTrace();
 	}
 
-	/**
-	 * 
-	 * Gets the question associated with this QuestionEditor.
-	 * 
-	 * @return the question
-	 */
+	questionDescription.setText(getQuestion().getMessage());
+	(getQuestion() instanceof FIBQuestion ? rb3
+		: (getQuestion() instanceof TFQuestion ? rb2 : rb1))
+		.setSelected(true);
+	blankSpaces.setModel(new SpinnerNumberModel(1, 1, 255, 1));
+	blankSpaces.addChangeListener(this);
 
-	public Question getQuestion()
+	// By default, the first is selected and is the "correct answer"
+	addChoice();
+
+	// Create a new layout which packs to each JPanel size, create a new
+	// panel with that layout, add all the components from the old to the
+	// new, then add the new panel
+	customLayout = new CustomCardLayout();
+	questionContainer = new JPanel(customLayout);
+
 	{
-		return question;
+	    int j = 0;
+	    for (Component i : tquestionContainer.getComponents())
+		questionContainer.add((JPanel) i, "" + j++);
 	}
 
-	private void addChoice()
+	remove(tquestionContainer);
+	add(questionContainer);
+
+	mcChoices.setModel(mcCListModel);
+	mcChoices.getSelectionModel().addListSelectionListener(this);
+
+	fibBlankList.setModel(fibBlankListModel);
+	fibBlankList.getSelectionModel().addListSelectionListener(this);
+
+	fibBlankCAChoicesList.setModel(fibCAListModel);
+	fibBlankCAChoicesList.getSelectionModel()
+		.addListSelectionListener(this);
+
+	setVisible(true);
+	pack();
+	pack();
+    }
+
+    /**
+     * 
+     * Gets the question associated with this QuestionEditor.
+     * 
+     * @return the question
+     */
+
+    public Question getQuestion()
+    {
+	return question;
+    }
+
+    /**
+     * 
+     * Adds a new blank to the JList and to the backend. Increments the
+     * fibBlankIndex to point to the new blank, and increment the fibBlankCount.
+     * Updates the buttons as necessary.
+     * 
+     */
+
+    private void addBlank()
+    {
+	fibQcorrectAnswers.add(++fibBlankIndex, new ArrayList<String>());
+	fibBlankListModel.add(fibBlankIndex, "Blank " + fibBlankIndex);
+	fibBlankCount++;
+
+	if (fibBlankCount >= 2)
+	    remB.setEnabled(true);
+    }
+
+    /**
+     * 
+     * Removes a blank from the JList and the backend. Decrements the
+     * fibBlankIndex if necessary, at the end of the list, and decrement the
+     * fibBlankCount. Updates the buttons as necessary.
+     * 
+     */
+
+    private void removeBlank()
+    {
+	if (fibBlankCount <= 1)
+	    return;
+
+	fibQcorrectAnswers.remove(fibBlankIndex);
+	fibBlankListModel.remove(fibBlankIndex);
+	fibBlankCount--;
+
+	if (fibBlankIndex == fibBlankCount)
+	    fibBlankIndex--;
+
+	if (fibBlankCount == 1)
+	    remB.setEnabled(false);
+    }
+
+    /**
+     * 
+     * Edits the current choice pointed to by the JList if the index is valid.
+     * Creates a new {@link JOptionPane} in order to achieve this.
+     * 
+     */
+
+    private void editChoice()
+    {
+	int editedIndex = mcChoiceIndex;
+
+	String msg = JOptionPane.showInputDialog(null,
+		"Please input choice message", "Edit Choice",
+		JOptionPane.QUESTION_MESSAGE);
+
+	mcQChoices.get(editedIndex).setMessage(msg);
+	mcCListModel.set(editedIndex, msg);
+    }
+
+    /**
+     * 
+     * Adds a new Choice to the JList and to the backend. Increments the
+     * mcChoiceIndex to point to the new choice, and increment the
+     * mcChoiceCount. Updates the buttons as necessary.
+     * 
+     */
+
+    private void addChoice()
+    {
+	Choice choice;
+	mcQChoices.add(++mcChoiceIndex, choice = new Choice("New Choice "
+		+ mcChoiceIndex));
+	mcCListModel.add(mcChoiceIndex, choice.getMessage());
+	mcChoiceCount++;
+
+	if (mcChoiceCount >= 2)
+	    remC.setEnabled(true);
+    }
+
+    /**
+     * 
+     * Removes a Choice from the JList and the backend. Decrements the
+     * mcChoiceIndex if necessary, at the end of the list, and decrement the
+     * mcChoiceCount. Updates the buttons as necessary.
+     * 
+     */
+
+    private void removeChoice()
+    {
+	if (mcChoiceCount <= 1)
+	    return;
+
+	mcQChoices.remove(mcChoiceIndex);
+	mcCListModel.remove(mcChoiceIndex);
+	mcChoiceCount--;
+
+	if (mcChoiceIndex == mcChoiceCount)
+	    mcChoiceIndex--;
+
+	if (mcChoiceCount == 1)
+	    remC.setEnabled(false);
+    }
+
+    /**
+     * 
+     * Moves a Choice up the JList. Cannot move a choice up that is at the
+     * beginning of the list.
+     * 
+     */
+
+    private void moveUpChoice()
+    {
+	if (mcChoiceIndex < 1)
+	    return;
+
+	Collections.swap(mcQChoices, mcChoiceIndex - 1, mcChoiceIndex);
+
+	String temp = (String) mcCListModel.get(mcChoiceIndex);
+	mcCListModel.set(mcChoiceIndex, mcCListModel.get(mcChoiceIndex - 1));
+	mcCListModel.set(mcChoiceIndex - 1, temp);
+
+	mcChoiceIndex--;
+	mcChoices.setSelectedIndex(mcChoiceIndex);
+    }
+
+    /**
+     * 
+     * Moves a Choice down the JList. Cannot move a choice down that is at the
+     * end of the list.
+     * 
+     */
+
+    private void moveDownChoice()
+    {
+	if (mcChoiceIndex == mcChoiceCount - 1)
+	    return;
+
+	Collections.swap(mcQChoices, mcChoiceIndex, mcChoiceIndex + 1);
+
+	String temp = (String) mcCListModel.get(mcChoiceIndex + 1);
+	mcCListModel.set(mcChoiceIndex + 1, mcCListModel.get(mcChoiceIndex));
+	mcCListModel.set(mcChoiceIndex, temp);
+
+	mcChoiceIndex++;
+	mcChoices.setSelectedIndex(mcChoiceIndex);
+    }
+
+    private void validateData()
+    {
+
+    }
+
+    /**
+     * 
+     * Disposes of this window and reenables the TestGenerator.
+     * 
+     */
+
+    private void exit()
+    {
+	// Change the type of question
+	Question newQuestion = rb1.isSelected() ? new MCQuestion(question)
+		: (rb2.isSelected() ? new TFQuestion(question)
+			: new FIBQuestion(question));
+
+	// Set message field
+	newQuestion.setMessage(questionDescription.getText());
+
+	if (newQuestion instanceof MCQuestion) // MC question attributes
 	{
+	    for (Choice i : mcQChoices)
+		// Add all the choices
+		((MCQuestion) newQuestion).addChoice(i);
+
+	    // Set the correct answer and if the question is mixable
+	    ((MCQuestion) newQuestion).setCorrectAnswer(mcCorrectAnswer);
+	    ((MCQuestion) newQuestion).setMixable(questionMixable.isSelected());
+	} else if (newQuestion instanceof TFQuestion) // TF question attributes
+	    // Sets the correct answer
+	    ((TFQuestion) newQuestion).setCorrectAnswer(rbTrue.isSelected());
+	else if (newQuestion instanceof FIBQuestion) // FIB question attributes
+	{
+	    // Add all the correct answers for all blanks
+	    for (int i = 0; i < fibQcorrectAnswers.size(); i++)
+		for (String j : fibQcorrectAnswers.get(i))
+		    ((FIBQuestion) newQuestion).addAnswer(i, j);
+
+	    // Set the number of blank spaces, if the question is case
+	    // sensitive, and if the question is whitespace sensitive
+	    ((FIBQuestion) newQuestion).setBlankSpaces((Integer) blankSpaces
+		    .getValue());
+	    ((FIBQuestion) newQuestion).setCaseSensitive(caseSensitive
+		    .isSelected());
+	    ((FIBQuestion) newQuestion)
+		    .setWhitespaceSensitive(whitespaceSensitive.isSelected());
 	}
 
-	/**
-	 * 
-	 * Disposes of this window and reenables the TestGenerator.
-	 * 
-	 */
+	// Make sure to update the question at the current index
+	TestGenerator.getInstance().updateQuestion(newQuestion);
 
-	private void exit()
+	// Switch to the other window
+	TestGenerator.getInstance().setVisible(true);
+	setVisible(false);
+	dispose();
+    }
+
+    /**
+     * 
+     * Change the question panel to the multiple choice question selection.
+     * 
+     */
+
+    private void chMCQPanel()
+    {
+	customLayout.show(questionContainer, "0");
+	pack();
+    }
+
+    /**
+     * 
+     * Change the question panel to the true/false question selection.
+     * 
+     */
+
+    private void chTFQPanel()
+    {
+	customLayout.show(questionContainer, "1");
+	pack();
+    }
+
+    /**
+     * 
+     * Change the question panel to the fill in the blank question selection.
+     * 
+     */
+
+    private void chFIBQPanel()
+    {
+	customLayout.show(questionContainer, "2");
+	pack();
+    }
+
+    /**
+     * 
+     * Change the question label to the multiple choice question selection.
+     * 
+     */
+
+    private void chMCQLabel()
+    {
+	questionLabel.setText("Create New Multiple Choice Question");
+    }
+
+    /**
+     * 
+     * Change the question label to the true/false question selection.
+     * 
+     */
+
+    private void chTFQLabel()
+    {
+	questionLabel.setText("Create New True/False Question");
+    }
+
+    /**
+     * 
+     * Change the question label to the fill in the blank question selection.
+     * 
+     */
+
+    private void chFIBQLabel()
+    {
+	questionLabel.setText("Create New Fill in the Blank Question");
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent event)
+    {
+	int bs = (Integer) blankSpaces.getValue();
+	System.out.println(bs);
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent event)
+    {
+	if (event.getSource() == mcChoices.getSelectionModel())
 	{
-		Question newQuestion = rb1.isSelected() ? new MCQuestion(question)
-				: (rb2.isSelected() ? new TFQuestion(question)
-						: new FIBQuestion(question));
+	    if (!event.getValueIsAdjusting()
+		    && mcChoices.getSelectedIndex() != -1)
+		mcChoiceIndex = mcChoices.getSelectedIndex();
 
-		newQuestion.setMessage(questionDescription.getText());
+	    moveUpC.setEnabled(true);
+	    moveDownC.setEnabled(true);
 
-		if (newQuestion instanceof MCQuestion)
-		{
-			for (Choice i : mcQChoices)
-				((MCQuestion) newQuestion).addChoice(i);
-
-			((MCQuestion) newQuestion).setCorrectAnswer(mcCorrectAnswer);
-			((MCQuestion) newQuestion).setMixable(mixable.isSelected());
-		} else if (newQuestion instanceof TFQuestion)
-			((TFQuestion) newQuestion).setCorrectAnswer(rbTrue.isSelected());
-		else if (newQuestion instanceof FIBQuestion)
-		{
-			for (int i = 0; i < fibQcorrectAnswers.size(); i++)
-				for (String j : fibQcorrectAnswers.get(i))
-					((FIBQuestion) newQuestion).addAnswer(i, j);
-			((FIBQuestion) newQuestion).setBlankSpaces((Integer) blankSpaces
-					.getValue());
-			((FIBQuestion) newQuestion).setCaseSensitive(caseSensitive
-					.isSelected());
-			((FIBQuestion) newQuestion)
-					.setWhitespaceSensitive(whitespaceSensitive.isSelected());
-		}
-
-		TestGenerator.getInstance().updateQuestion(newQuestion);
-
-		TestGenerator.getInstance().setVisible(true);
-		setVisible(false);
-		dispose();
+	    if (mcChoiceIndex == 0)
+		moveUpC.setEnabled(false);
+	    if (mcChoiceIndex == mcChoiceCount - 1)
+		moveDownC.setEnabled(false);
 	}
-
-	private void doNothing()
-	{
-	}
-
-	/**
-	 * 
-	 * Change the question panel to the multiple choice question selection.
-	 * 
-	 */
-
-	private void chMCQPanel()
-	{
-		questionLayout.show(questionContainer, "MCQuestionContainer");
-	}
-
-	/**
-	 * 
-	 * Change the question panel to the true/false question selection.
-	 * 
-	 */
-
-	private void chTFQPanel()
-	{
-		questionLayout.show(questionContainer, "TFQuestionContainer");
-	}
-
-	/**
-	 * 
-	 * Change the question panel to the fill in the blank question selection.
-	 * 
-	 */
-
-	private void chFIBQPanel()
-	{
-		questionLayout.show(questionContainer, "FIBQuestionContainer");
-	}
-
-	/**
-	 * 
-	 * Change the question label to the multiple choice question selection.
-	 * 
-	 */
-
-	private void chMCQLabel()
-	{
-		questionLabel.setText("Create New Multiple Choice Question");
-	}
-
-	/**
-	 * 
-	 * Change the question label to the true/false question selection.
-	 * 
-	 */
-
-	private void chTFQLabel()
-	{
-		questionLabel.setText("Create New True/False Question");
-	}
-
-	/**
-	 * 
-	 * Change the question label to the fill in the blank question selection.
-	 * 
-	 */
-
-	private void chFIBQLabel()
-	{
-		questionLabel.setText("Create New Fill in the Blank Question");
-	}
-
-	@Override
-	public void stateChanged(ChangeEvent event)
-	{
-		int bs = (Integer) blankSpaces.getValue();
-		
-	}
+    }
 }
