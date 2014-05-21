a644a8a70f879c916c4fc86ca0adeab507689c54
Jonathan Ni <jonathan.r.ni@gmail.com>
2014-05-21 12:13:11 -0400

    Edit TestTaker GUI

--- ClientProject/src-client/com/esf/tm/client/TestTaker.java
+++ ClientProject/src-client/com/esf/tm/client/TestTaker.java
@@ -1,12 +1,9 @@
 package com.esf.tm.client;
 
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-
 import javax.swing.JFrame;
+
+import org.javabuilders.BuildResult;
+import org.javabuilders.swing.SwingJavaBuilder;
 
 /**
  * 
@@ -14,103 +11,48 @@ import javax.swing.JFrame;
  * 
  * @author Jonathan Ni
  * @since 4/26/14
- * @version 0.0r1
+ * @version 0.0r2
  * 
  */
 
-public class TestTaker extends JFrame implements WindowListener, ActionListener
+public class TestTaker extends JFrame
 {
-	private static final long serialVersionUID = -5547354766805951582L;
+    private static final long serialVersionUID = -5547354766805951582L;
 
-	private static final int WIDTH = 640, HEIGHT = 480;
-	private static final int SCREEN_WIDTH = (int) Toolkit.getDefaultToolkit()
-			.getScreenSize().getWidth(), SCREEN_HEIGHT = (int) Toolkit
-			.getDefaultToolkit().getScreenSize().getHeight();
+    private BuildResult result;
 
-	/**
-	 * 
-	 * Creates a new test generator message panel to be used by the teacher.
-	 * 
-	 */
+    /**
+     * 
+     * Creates a new test generator message panel to be used by the teacher.
+     * 
+     */
 
-	public TestTaker()
-	{
-		super();
+    public TestTaker()
+    {
+	super();
 
-		// Formatting
-		setSize(WIDTH, HEIGHT);
-		setLocation(SCREEN_WIDTH / 2 - WIDTH / 2, SCREEN_HEIGHT / 2 - HEIGHT
-				/ 2);
-		setVisible(true);
-		addWindowListener(this);
-	}
+	result = SwingJavaBuilder.build(this);
 
-	@Override
-	public void windowActivated(WindowEvent we)
-	{
+	pack();
+	setVisible(true);
+	pack();
+    }
 
-	}
+    /**
+     * 
+     * Destroys the program.
+     * 
+     * @param callback
+     *            the "function" to call before exiting.
+     */
+    static void destroy(Callback callback)
+    {
+	callback.cbFunction();
+	System.exit(0);
+    }
 
-	@Override
-	public void windowClosed(WindowEvent we)
-	{
-
-	}
-
-	@Override
-	public void windowClosing(WindowEvent we)
-	{
-
-	}
-
-	@Override
-	public void windowDeactivated(WindowEvent we)
-	{
-
-	}
-
-	@Override
-	public void windowDeiconified(WindowEvent we)
-	{
-
-	}
-
-	@Override
-	public void windowIconified(WindowEvent we)
-	{
-
-	}
-
-	@Override
-	public void windowOpened(WindowEvent we)
-	{
-
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e)
-	{
-
-	}
-
-	/**
-	 * 
-	 * Destroys the program.
-	 * 
-	 * @param callback
-	 *            the "function" to call before exiting.
-	 */
-	static void destroy(Callback callback)
-	{
-		callback.cbFunction();
-		System.exit(0);
-	}
-
-	public static void main(String[] args)
-	{
-		if (SCREEN_WIDTH < 0 || SCREEN_HEIGHT < 0)
-			ErrorReporter.reportError(
-					"Error occured while initiating graphics", "");
-		new TestTaker();
-	}
+    public static void main(String[] args)
+    {
+	new TestTaker();
+    }
 }
